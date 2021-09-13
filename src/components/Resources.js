817de153acc8841b7db6cb7ee26bbf85b0c1da23
y inoue <admin@wainoMacBook-Pro.local>
2021-09-13 18:26:23 +0900

    amend resource

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Resources = ({ resources}) => {
-    return (
-        <React.Fragment>
-            {resources.map((resource) =>{
-                <p key={resource.id}>{resource.title}</p>
-             })}
-        </React.Fragment>
-    );
+const Resources = ({ resources }) => {
+  return (
+    <React.Fragment>
+      {resources.map((resource) => {
+        return <p key={resource.id}>{resource.title}</p>;
+      })}
+    </React.Fragment>
+  );
 };
 
 export default Resources;
